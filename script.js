e1ef8b11a4a04cc305cf986af0ceadbf15de9cfc
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-08-20 20:22:27 -0400

    complete function for runAnimationIN

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const blackBar = document.querySelector(".black-bar");
 const bottomBar = document.querySelector(".bottom-bar");
 const textLayer = document.querySelector(".main-bar-text");
 const bottomBarText = document.querySelector(".bottom-bar .bottom-bar-text");
+const logo = document.querySelector(".img-container");
+const topBar = document.querySelector(".top-bar");
+
+// FUNCTION: RUN ANIMATION 
+function runAnimationIN() {
+  // Re-run animations
+  logo.style.animation = "revealLeft 0.5s linear forwards";
+  goldBar.style.animation = "slideIn 0.5s ease-in-out forwards 0.1s";
+  blackBar.style.animation = "slideIn 0.5s ease-in-out forwards 0.1s";
+  bottomBar.style.animation = "slideDownBottom 0.6s ease-in-out forwards 0.2s";
+  topBar.style.animation = "slideUpTop 0.5s ease-in forwards 0.2s";
+}
 
 // ON LOAD: adjust bar width
 window.onload = () => adjustBarWidth();
